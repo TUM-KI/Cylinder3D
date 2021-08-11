0a768b6c54135f01a687a8bf04c85fe266d4a247
Daniel Derkacz <mail@daniel-derkacz.de>
2021-08-11 16:22:21 +0200

    changed python -> python3 in train_nusc.sh

--- train_nusc.sh
+++ train_nusc.sh
@@ -1,5 +1,5 @@
 name=cylinder_asym_networks_nusc
 gpuid=0
 
-CUDA_VISIBLE_DEVICES=${gpuid}  python -u train_cylinder_asym_nuscenes.py \
+CUDA_VISIBLE_DEVICES=${gpuid}  python3 -u train_cylinder_asym_nuscenes.py \
 2>&1 | tee logs_dir/${name}_logs_tee.txt
