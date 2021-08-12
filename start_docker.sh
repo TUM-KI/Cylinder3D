4ae456cd90707a1af8d1ab7b29fe0e7e6714d83d
Daniel Derkacz <mail@daniel-derkacz.de>
2021-08-12 17:14:50 +0200

    changes in start_docker script

--- start_docker.sh
+++ start_docker.sh
@@ -5,9 +5,11 @@ containername=cylinder3d
 manjaro="Manjaro Linux"
 
 docker build -t $containername .
-#echo $distroname
-#if [[ $distroname == $manjaro ]]; then 
-#    docker run --rm --gpus all -it --privileged -v /dev:/dev -v $datafolder:/data/dataset/nuScenes $containername bash
-#else
-    docker run --rm --gpus all -it -v $datafolder:/data/dataset/nuScenes --shm-size 16G $containername bash 
-#fi
+# For Manjaro use this:
+#    docker run --rm --gpus all -it --privileged -v /dev:/dev -v $datafolder:/data/dataset/nuScenes --shm-size 16G $containername bash
+
+# For Ubuntu 20 use this
+docker run --rm --gpus all -it -v $datafolder:/data/dataset/nuScenes --shm-size 16G $containername bash 
+
+# For Ubuntu 16 use this
+# docker run --rm --runtime=nvidia -it -v $datafolder:/data/dataset/nuScenes --shm-size 16G $containername bash
