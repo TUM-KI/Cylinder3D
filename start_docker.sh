bf48e145a6e314b2d7bef9e2b548c55dfe71d4f0
Daniel Derkacz <mail@daniel-derkacz.de>
2021-08-21 13:30:29 +0200

    changes in start script

--- start_docker.sh
+++ start_docker.sh
@@ -3,13 +3,15 @@ distroname="$(lsb_release -s -d)"
 datafolder=/home/derkacz/ki-al/datasets/nuscenes/new
 resultfolder=/home/derkacz/ki-al/datasets/results
 modelfolder=/home/derkacz/ki-al/datasets/nuscenes
+savefolder=/home/derkacz/ki-al/datasets/nuscenes/save
 containername=cylinder3d
 manjaro="Manjaro Linux"
 
 docker build -t $containername .
 # For Manjaro use this:
 docker run --rm --gpus all -it --privileged -v /dev:/dev -v $datafolder:/data/dataset/nuScenes --shm-size 16G \
-    -v $resultfolder:/data/datasets/results -v $modelfolder:/cylinder3d/model_load_dir_nuscenes $containername python3 test.py
+    -v $resultfolder:/data/datasets/results -v $modelfolder:/cylinder3d/model_load_dir_nuscenes \
+    -v $savefolder:/cylinder3d/model_save_dir_nuscenes $containername bash 
 
 # For Ubuntu 20 use this
 #docker run --rm --gpus all -it -v $datafolder:/data/dataset/nuScenes --shm-size 16G $containername bash 
