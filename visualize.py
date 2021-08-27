d1bb351144c44df362a69171cab42f543ee21397
Daniel Derkacz <mail@daniel-derkacz.de>
2021-08-27 15:02:02 +0200

    save pointcloud

--- visualize.py
+++ visualize.py
@@ -7,11 +7,51 @@ import sys
 
 from config.config import load_config_data
 from builder import data_builder, model_builder
+from dataloader.pc_dataset import get_nuScenes_label_name, get_nuScenes_colormap
+
+
+from plyfile import PlyData, PlyElement
+
+def save_point_cloud(pc, predicted_label,groundtruth_label, colormap):
+    num_vertices = pc.shape[0]
+    vertices = np.zeros((num_vertices), dtype=[('x', np.float32), ('y', np.float32),
+    ('z', np.float32), ('red', np.ubyte), ('green', np.ubyte), ('blue', np.ubyte)])
+
+    vertices2 = np.zeros((num_vertices), dtype=[('x', np.float32), ('y', np.float32),
+    ('z', np.float32), ('red', np.ubyte), ('green', np.ubyte), ('blue', np.ubyte)])
+
+
+    unique_label_prediction = np.unique(predicted_label)
+    unique_label_groundtruth = np.unique(groundtruth_label[:,0])
+    prediction_bin_count = np.bincount(predicted_label)[unique_label_prediction]
+    groundtruth_bin_count = np.bincount(groundtruth_label[:,0])[unique_label_groundtruth]
+    
+    print(f"unique predicted {set(zip(unique_label_prediction, prediction_bin_count))}")
+    print(f"unique groundtruth {set(zip(unique_label_groundtruth, groundtruth_bin_count))}")
+    for index in range(pc.shape[0]):
+        point = pc[index,:]
+        l = predicted_label[index]
+        g = groundtruth_label[index,0]
+        c = colormap[l]
+        gc = colormap[g]
+        vertices[index] = (*point, *c)
+        vertices2[index] = (*point, *gc)
+    print(vertices)
+    el = PlyElement.describe(vertices, 'vertex')
+    el2 = PlyElement.describe(vertices2, 'vertex')
+    PlyData([el], text=True).write('tmp/test.ply')
+    PlyData([el2], text=True).write('tmp/groundtruth.ply')
+
 
 def main(args):
     pytorch_device = torch.device('cuda:0')
 
     configs = load_config_data(args.config_path)
+
+    label_mapping = configs['dataset_params']['label_mapping']
+    label_name = get_nuScenes_label_name(label_mapping)
+    label_colormap = get_nuScenes_colormap(label_mapping)
+    print(label_name)
 
     model_config = configs['model_params']
     my_model = model_builder.build(model_config)
@@ -39,7 +79,25 @@ def main(args):
             val_label_tensor = val_vox_label.type(torch.LongTensor).to(pytorch_device)
 
             predicted_labels = my_model(val_pt_fea_tensor, val_grid_tensor, val_batch_size)
-            print(predicted_labels)
+            predicted_labels = torch.argmax(predicted_labels, dim=1)
+            predicted_labels = predicted_labels.cpu().detach().numpy()
+            # print(predicted_labels[0])
+
+            #ego_transform, csr_transform = val_dataset_loader.get_transform
+            print(f"shape of val_grid: {val_grid[0].shape}")
+
+            for count, _ in enumerate(val_grid):
+                label = predicted_labels[
+                    count, val_grid[count][:,0], 
+                    val_grid[count][:,1],
+                    val_grid[count][:,2]
+                ]
+                pointcloud = val_grid[count]
+                groundtruth = val_pt_labs[count]
+                save_point_cloud(pointcloud, label, groundtruth, label_colormap)
+                break
+
+            break
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
